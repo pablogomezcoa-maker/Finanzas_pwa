2be9ca468de8eb25a29e18f0127a37c31b4bed4a
pablogomezcoa-maker <pablogomezcoa@gmail.com>
2026-07-31 20:40:40 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'finanzas-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+// Red primero: siempre intenta traer la versión más nueva. Si no hay internet,
+// recién ahí usa la copia guardada. Así las actualizaciones se ven al tiro.
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(event.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+  );
+});
